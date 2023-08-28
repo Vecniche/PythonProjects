4d1308f167c6328e212893634fe5ba62acce716f
Onwuka Obinna <99493485+Vecniche@users.noreply.github.com>
2023-08-28 07:05:09 -0700

    initial commit

---
+++ Tic_Tac_Toe/main.py
@@ -0,0 +1,52 @@
+def print_board(board):
+    for row in board:
+        print(" | ".join(row))
+        print("-" * 9)
+
+
+def check_winner(board, player):
+    # I did this to Check rows, columns, and diagonals for a win
+    for i in range(3):
+        if all(board[i][j] == player for j in range(3)) or all(board[j][i] == player for j in range(3)):
+            return True
+    if all(board[i][i] == player for i in range(3)) or all(board[i][2 - i] == player for i in range(3)):
+        return True
+    return False
+
+
+def is_draw(board):
+    return all(cell != " " for row in board for cell in row)
+
+
+def main():
+    board = [[" " for _ in range(3)] for _ in range(3)]
+    players = ["X", "O"]
+    current_player = 0
+
+    while True:
+        print_board(board)
+        print(f"Player {players[current_player]}'s turn")
+
+        row = int(input("Enter row (0, 1, or 2): "))
+        col = int(input("Enter column (0, 1, or 2): "))
+
+        if 0 <= row < 3 and 0 <= col < 3 and board[row][col] == " ":
+            board[row][col] = players[current_player]
+
+            if check_winner(board, players[current_player]):
+                print_board(board)
+                print(f"Player {players[current_player]} wins!")
+                break
+
+            if is_draw(board):
+                print_board(board)
+                print("It's a draw!")
+                break
+
+            current_player = 1 - current_player
+        else:
+            print("Invalid move. Try again.")
+
+
+if __name__ == "__main__":
+    main()
